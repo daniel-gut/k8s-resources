43ae969f4a3fd8efea6fabcfbe69ad9f543161a1
daniel-gut <daniel_gut@outlook.com>
2020-07-17 11:37:40 +0200

    fix path to file

--- scenarios/pod-with-pvc.sh
+++ scenarios/pod-with-pvc.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
-kubectl apply -f https://raw.githubusercontent.com/daniel-gut/kure/k8s-resources/master/resources/namespace.yaml
-kubectl apply -f https://raw.githubusercontent.com/daniel-gut/kure/k8s-resources/master/resources/storage/storageclass-gke.yaml
-kubectl apply -f https://raw.githubusercontent.com/daniel-gut/kure/k8s-resources/master/resources/storage/pvc.yaml
-kubectl apply -f https://raw.githubusercontent.com/daniel-gut/kure/k8s-resources/master/resources/apps/pod.yaml
+kubectl apply -f https://raw.githubusercontent.com/daniel-gut/k8s-resources/master/resources/namespace.yaml
+kubectl apply -f https://raw.githubusercontent.com/daniel-gut/k8s-resources/master/resources/storage/storageclass-gke.yaml
+kubectl apply -f https://raw.githubusercontent.com/daniel-gut/k8s-resources/master/resources/storage/pvc.yaml
+kubectl apply -f https://raw.githubusercontent.com/daniel-gut/k8s-resources/master/resources/apps/pod.yaml
 
